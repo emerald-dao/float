531f611a94affc64c80db49619b25527e7ecc8cf
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-01-26 15:54:00 +0100

    removed ssr

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const response = await resolve(event, {
-		ssr: true
+		ssr: false
 	});
 
 	return response;
