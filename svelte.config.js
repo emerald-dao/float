19a493b680f478fcaeab9ac700e8671731ce72a0
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-02-06 21:21:01 +0100

    trying some things for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -39,7 +39,7 @@ const config = {
         globals()
       ],
       optimizeDeps: {
-        exclude: ['electron', 'electron-fetch', 'ipfs-http-client']
+        exclude: ['electron-fetch', 'ipfs-http-client']
       }
     },
   },
