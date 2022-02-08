125eacac9b8709fd850d5035a60c51558a3b5b8c
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-02-08 09:32:04 +0100

    added meta component to update title and meta tags within nested component

---
+++ src/lib/components/common/Meta.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { PAGE_TITLE_EXTENSION } from '$lib/constants';
+  export let title;
+  export let author;
+  export let description;
+
+</script>
+<svelte:head>
+  <title>{title} {PAGE_TITLE_EXTENSION}</title>
+  <meta name="author" content="{author}">
+  <meta name="description" content="{description}">
+</svelte:head>
