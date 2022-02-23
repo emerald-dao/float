18397f5fca431b53c5bfadc618beadc1b3a9b1d5
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-02-23 14:39:18 +0100

    improved serial page

--- src/routes/[address]/[eventId]/[serial].svelte
+++ src/routes/[address]/[eventId]/[serial].svelte
@@ -1,12 +1,13 @@
 <script>
   import { page } from "$app/stores";
+  import { browser } from '$app/env';
   import Loading from "$lib/components/common/Loading.svelte";
   import Float from "$lib/components/Float.svelte";
   import { getEvent, getFLOAT, transferFLOAT, getCurrentHolder, deleteFLOAT } from "$lib/flow/actions.js";
   import Meta from '$lib/components/common/Meta.svelte';
   import { user } from "$lib/flow/stores";
   import { onMount } from "svelte";
-
+  
   const floatEventCallback = async () => {
     return new Promise(async (resolve, reject) => {
       let event = await getEvent($page.params.address, $page.params.eventId);
@@ -18,7 +19,7 @@
   }
   let recipientAddr = "";
   let data = floatEventCallback();
-
+  
   // JS STUFF
   let card;
   let container;
@@ -26,20 +27,28 @@
   let image;
   let createdBy;
   let serial;
-
-  onMount(() => {
+  
+  $: if(card, container, title, image, createdBy, serial) {
+    // once dom elements are loaded, fire animate
+    animate(container)
+    // then delete animate as it would keep firing
+    animate = () => {}
+  }
+  
+  
+  function animate(container) {
     container.addEventListener('mousemove', (e) => {
       let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
       let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
       card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     });
-
+    
     container.addEventListener('touchmove', (e) => {
       let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
       let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
       card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     });
-
+    
     container.addEventListener('mouseenter', (e) => {
       setTimeout(() => {
         card.style.transition = 'none';
@@ -49,7 +58,7 @@
       createdBy.style.transform = 'translateZ(125px)';
       serial.style.transform = 'translateZ(100px)';
     });
-
+    
     container.addEventListener('mouseleave', (e) => {
       card.style.transition = 'all 0.5s ease';
       card.style.transform = `rotateY(0deg) rotateX(0deg)`;
@@ -58,99 +67,103 @@
       createdBy.style.transform = 'translateZ(0px)';
       serial.style.transform = 'translateZ(0px)';
     });
-  });
-
+    
+  }
 </script>
 
 {#await data then data}
 <Meta
-  title="{data?.event.name} | FLOAT #{$page.params.eventId}"
-  author={data?.event.host}
-  description={data?.event.description}
-  url={$page.url}
+title="{data?.event.name} | FLOAT #{$page.params.eventId}"
+author={data?.event.host}
+description={data?.event.description}
+url={$page.url}
 />
 {/await}
 
-<div class="whole">
-  <div class="wrap" bind:this={container}>
-    <div class="float" bind:this={card}>
-      {#await data then data}
-        <div class="image" bind:this={image}>
+{#await data then data}
+<article class="toggle">
+  <header>
+    {#if data?.float.owner}
+    <h3>Owned by {data?.float.owner === $user.addr ? "you" : data?.float.owner}</h3>
+    <small class="muted">Originally claimed by {data?.float.originalRecipient}</small>
+    {:else}
+    <h3>This FLOAT was deleted.</h3>
+    <small class="muted">Original Recipient: Unknown</small>
+    {/if}
+  </header>
+  
+  <div class="whole">
+    <div class="wrap" bind:this={container}>
+      <div class="float transition" bind:this={card}>
+        <div class="image transition" bind:this={image}>
           <img src={`https://ipfs.infura.io/ipfs/${data.event.image}`} alt="float">
         </div>
         <div class="info">
-          <h1 bind:this={title}>{data.event.name}</h1>
-          <p bind:this={createdBy}>
+          <h1 class="transition" bind:this={title}>{data.event.name}</h1>
+          <p class="transition" bind:this={createdBy}>
             <small>
               <span class="credit">Created by</span>
               <a href="/{data.event.host}" class="host">{data.event.host}</a>
             </small>
           </p>
-          <code data-tooltip="Serial #" bind:this={serial}>#{data.float.serial}</code>
+          <code class="transition" data-tooltip="Serial #" bind:this={serial}>#{data.float.serial}</code>
         </div>
-      {/await}
+      </div>
     </div>
   </div>
-</div>
+  
+  <blockquote>
+    <strong><small class="muted">DESCRIPTION</small></strong><br/>
+    {data?.event.description}
+  </blockquote>
+  {#if $user?.addr == data?.float.owner}
+  <button class="outline red" on:click={() => deleteFLOAT(data?.float.id)}>
+    Delete this FLOAT
+  </button>
+  {/if}
+</article>
 
-{#await data then data}
-    <article class="toggle">
-      <header>
-        {#if data?.float.owner}
-          <h3>Owned by {data?.float.owner === $user.addr ? "you" : data?.float.owner}</h3>
-          <small class="muted">Originally received by {data?.float.originalRecipient}</small>
-        {:else}
-          <h3>This FLOAT was deleted.</h3>
-          <small class="muted">Original Recipient: Unknown</small>
-        {/if}
-      </header>
-      <blockquote>
-        <strong><small class="muted">DESCRIPTION</small></strong><br/>
-        {data?.event.description}
-      </blockquote>
-      {#if $user?.addr == data?.float.owner}
-        <button class="outline red" on:click={() => deleteFLOAT(data?.float.id)}>
-          Delete this FLOAT
-        </button>
-      {/if}
-    </article>
-    {#if $user?.addr == data?.float.owner}
-      <article>
-        <label for="recipientAddr">
-          Copy the recipient's address below.
-          <input
-            type="text"
-            name="recipientAddr"
-            bind:value={recipientAddr}
-            placeholder="0x00000000000"
-          />
-        </label>
-        <button on:click={transferFLOAT(data?.float.id, recipientAddr)}>Transfer this FLOAT</button>
-      </article>
-    {/if}
+{#if $user?.addr == data?.float.owner}
+<article>
+  <label for="recipientAddr">
+    Copy the recipient's address below.
+    <input
+    type="text"
+    name="recipientAddr"
+    bind:value={recipientAddr}
+    placeholder="0x00000000000"
+    />
+  </label>
+  <button on:click={transferFLOAT(data?.float.id, recipientAddr)}>Transfer this FLOAT</button>
+</article>
+{/if}
 {/await}
 
 <style>
   .whole {
     position: relative;
     display: flex;
+    width: 100%;
+    height:100%;
     justify-content: center;
     align-items: center;
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     left: 0;
+    z-index: 10000;
   }
-
+  
   .wrap {
     /* Body Stuff */
     width: 50%;
     display: flex;
+    position:relatve;
     justify-content: center;
     align-items: center;
     perspective: 1000px;
   }
-
+  
   .float {
     transform-style: preserve-3d;
     width: 20rem;
@@ -161,42 +174,42 @@
     box-shadow: 0 20px 20px rgb(0, 0, 0, 0.2), 0 0px 50px rgb(0, 0, 0, 0.2);
     background-color: var(--card-background-color);
   }
-
+  
   .image {
     min-height: 35vh;
     display: flex;
     align-items: center;
     justify-content: center;
   }
-
+  
   .image img {
     width: 20rem;
   }
-
+  
   .info {
     text-align: center;
     transform-style: preserve-3d;
   }
-
+  
   .info h1 {
     font-size: 1.5rem;
   }
-
+  
   .credit {
     margin-top: 20px;
     display: block;
   }
-
+  
   .host {
     font-family: monospace;
   }
-
+  
   article {
     text-align: center;
     align-items: center;
-  
+    
   }
-
+  
   .muted {
     font-size: 0.7rem;
     opacity: 0.7;
@@ -204,5 +217,10 @@
   blockquote {
     text-align: left;
   }
-
+  
+  .transition {
+    transition-property: transform;
+    transition-duration: 1s;
+  }
+  
 </style>
