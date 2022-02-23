26ca4c4d437f72970641fb48ae534d4a93d597d8
Jacob Tucker <tsnakejake@aol.com>
2022-02-23 04:09:17 -0600

    touchmove

--- src/routes/[address]/[eventId]/[serial].svelte
+++ src/routes/[address]/[eventId]/[serial].svelte
@@ -34,6 +34,12 @@
       card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     });
 
+    container.addEventListener('touchmove', (e) => {
+      let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+      let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+      card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+
     container.addEventListener('mouseenter', (e) => {
       setTimeout(() => {
         card.style.transition = 'none';
