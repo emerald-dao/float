86a86d1907c58f249b4e8b3a7f2488b2e6a83f55
jacob-tucker <jacobtucker818@gmail.com>
2022-04-03 17:52:41 -0500

    add config addresses back

--- src/lib/flow/config.js
+++ src/lib/flow/config.js
@@ -1,12 +1,12 @@
-import {config} from "@samatech/onflow-fcl-esm";
+import { config } from "@samatech/onflow-fcl-esm";
 
 config({
   "app.detail.title": "FLOAT",
   "app.detail.icon": "https://i.imgur.com/WJMROey.png",
   "accessNode.api": import.meta.env.VITE_ACCESS_NODE_API,
   "discovery.wallet": import.meta.env.VITE_DISCOVERY_WALLET,
-  "0xFLOAT": import.meta.env.VITE_FLOAT_ADDRESS,
-  "0xCORE": import.meta.env.VITE_CORE_ADDRESS,
-  "0xFN": import.meta.env.VITE_FN_ADDRESS,
-  "0xFIND": import.meta.env.VITE_FIND_ADDRESS
+  "0xFLOAT": "0x0afe396ebc8eee65",
+  "0xCORE": "0x631e88ae7f1d7c20",
+  "0xFN": "0xb05b2abb42335e88",
+  "0xFIND": "0xa16ab1d0abde3625"
 })
