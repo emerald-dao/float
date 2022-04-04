2c1b4dbebd03d07d645ed3bedf89f490d3aa0aaf
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-04-04 15:44:15 +0200

    uncommented live

--- src/routes/live.svelte
+++ src/routes/live.svelte
@@ -1,6 +1,6 @@
-<!-- <script>
+<script>
   import Lantern from "$lib/components/Lantern.svelte";
-  //import LibLoader from '$lib/components/LibLoader.svelte';
+  import LibLoader from '$lib/components/LibLoader.svelte';
   import GraffleSDK from "$lib/graffle.js";
   import { onMount } from "svelte";
 
@@ -34,23 +34,22 @@
     console.log("Creating the stream");
     streamSDK.stream(receiveEvent);
   });
-</script> -->
+</script>
 
-<!-- 
 <LibLoader
   url="https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/6.0.2/signalr.min.js"
   on:loaded={() => createStream()}
   uniqueId={+new Date()}
-/> -->
+/>
 
-<!-- <div id="spawner">
+<div id="spawner">
   <img class="island" src="/island.png" alt="FLOATing island" />
   {#each Object.keys(claimedEvents) as id (id)}
     <Lantern ipfsHash={claimedEvents[id]} />
   {/each}
-</div> -->
+</div>
 
-<!-- <style>
+<style>
   div {
     text-align: center;
   }
@@ -90,4 +89,4 @@
       transform: translateY(0);
     }
   }
-</style> -->
+</style>
