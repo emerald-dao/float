7894fea59b983717086a89eb91bac8326200a20d
Jacob Tucker <tsnakejake@aol.com>
2022-04-04 09:08:11 -0500

    mainnet

--- src/lib/flow/config.js
+++ src/lib/flow/config.js
@@ -3,8 +3,8 @@ import { config } from "@samatech/onflow-fcl-esm";
 config({
   "accessNode.api": import.meta.env.VITE_ACCESS_NODE_API,
   "discovery.wallet": import.meta.env.VITE_DISCOVERY_WALLET,
-  "0xFLOAT": "0x0afe396ebc8eee65",
-  "0xCORE": "0x631e88ae7f1d7c20",
-  "0xFN": "0xb05b2abb42335e88",
-  "0xFIND": "0xa16ab1d0abde3625"
+  "0xFLOAT": "0x2d4c3caffbeab845",
+  "0xCORE": "0x1d7e57aa55817448",
+  "0xFN": "0x233eb012d34b0070",
+  "0xFIND": "0x097bafa4e0b48eef"
 })
