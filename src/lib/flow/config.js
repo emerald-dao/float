899465e88a6d30780a505d7b01fda1fddc264270
Jacob Tucker <tsnakejake@aol.com>
2022-04-03 22:04:58 -0500

    hardcodeeeeeee

--- src/lib/flow/config.js
+++ src/lib/flow/config.js
@@ -3,8 +3,8 @@ import { config } from "@samatech/onflow-fcl-esm";
 config({
   "app.detail.title": "FLOAT",
   "app.detail.icon": "https://i.imgur.com/WJMROey.png",
-  "accessNode.api": import.meta.env.VITE_ACCESS_NODE_API,
-  "discovery.wallet": import.meta.env.VITE_DISCOVERY_WALLET,
+  "accessNode.api": "https://testnet.onflow.org",
+  "discovery.wallet": "http://localhost:8701/fcl/authn",
   "0xFLOAT": "0x0afe396ebc8eee65",
   "0xCORE": "0x631e88ae7f1d7c20",
   "0xFN": "0xb05b2abb42335e88",
