b4fb4e531fe4ec9f1f3ed4a72859e69515166854
jacob-tucker <jacobtucker818@gmail.com>
2022-04-05 17:09:00 -0500

    change wording

--- src/lib/components/CheckAddress.svelte
+++ src/lib/components/CheckAddress.svelte
@@ -1,16 +1,21 @@
 <script>
   let address;
 
-  const onKeyPress = e => {
-    if (e.charCode === 13) window.location = "/"+address;
+  const onKeyPress = (e) => {
+    if (e.charCode === 13) window.location = "/" + address;
   };
 </script>
 
 <div class="input-button-group">
-  <input type="text" id="address" name="address" placeholder="0x00000000000" bind:value={address} on:keypress="{onKeyPress}">
+  <input
+    type="text"
+    id="address"
+    name="address"
+    placeholder="0x00000000000"
+    bind:value={address}
+    on:keypress={onKeyPress} />
   <a href="/{address}" role="button" class:disabled={address}>View</a>
 </div>
-<small>Paste in a Flow address.</small>
-  
-  <!-- Button -->
-  
+<small>Paste in a Flow address, .find, or .fn name.</small>
+
+<!-- Button -->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,45 @@
 <script>
-  import CheckAddress from '$lib/components/CheckAddress.svelte';
-  import { PAGE_TITLE_EXTENSION } from '$lib/constants';
+  import CheckAddress from "$lib/components/CheckAddress.svelte";
+  import { PAGE_TITLE_EXTENSION } from "$lib/constants";
 </script>
 
+<svelte:head>
+  <title>Home {PAGE_TITLE_EXTENSION}</title>
+</svelte:head>
+
+<div class="grid">
+  <a href="/live">
+    <div class="text-center">
+      <img class="island" src="/island.png" alt="FLOAT Logo" />
+    </div>
+  </a>
+  <div class="mt-2 mb-2 text-center">
+    <h1>Welcome to FLOAT</h1>
+    <small
+      >A service by <a href="https://ecdao.org" target="_blank"
+        ><img
+          style="height:20px;width:auto;margin-right:4px;"
+          src="/emeraldcitylogo.png"
+          alt="Emerald Logo" />Emerald City DAO</a
+      ></small>
+    <span class="tag-line"
+      >Claim a unique NFT to show that you attended an event.</span>
+
+    <div class="grid mt-2 no-break align-center">
+      <a role="button" href="/live" class="outline small-button mb-1"
+        >Watch live feed</a>
+      <a role="button" href="/create" class="small-button mb-1"
+        >Create an event</a>
+    </div>
+  </div>
+</div>
+
+<article>
+  <h2 id="findAddress">View FLOATs from an Account</h2>
+  <CheckAddress />
+</article>
+
 <style>
-  
   /* video {
     display:block;
     width:100%;
@@ -12,16 +47,16 @@
     margin: 20px auto;
   }
    */
-  
+
   h1 {
-    text-align:center;
+    text-align: center;
   }
 
   .tag-line {
     font-weight: 300;
     font-size: 1.2rem;
     margin-top: 30px;
-    display:block;
+    display: block;
   }
 
   .island {
@@ -29,35 +64,3 @@
     max-width: 400px;
   }
 </style>
-
-<svelte:head>
-<title>Home {PAGE_TITLE_EXTENSION}</title>
-</svelte:head>
-
-<!-- <video autoplay muted loop id="myVideo">
-  <source src="island.mp4" type="video/mp4" />
-</video> -->
-
-<div class="grid">
-  <a href="/live">
-    <div class="text-center">
-      <img  class="island" src="/island.png" alt="FLOAT Logo" />
-    </div>
-  </a>
-  <div class="mt-2 mb-2 text-center">
-    <h1>Welcome to FLOAT</h1>
-    <small>A service by <a href="https://ecdao.org" target="_blank"><img style="height:20px;width:auto;margin-right:4px;" src="/emeraldcitylogo.png" alt="Emerald Logo" />Emerald City DAO</a></small>
-    <span class="tag-line">Unlock a unique identifier to show that you attended an event.</span>
-
-    <div class="grid mt-2 no-break align-center">
-      <a role="button" href="/live" class="outline small-button mb-1">Watch live feed</a>
-      <a role="button" href="/create" class="small-button mb-1">Create an event</a>
-    </div>
-  </div>
-</div>
-
-<article>
-  <h2 id="findAddress">View FLOATs from an Address</h2>
-  <CheckAddress />
-</article>
-
