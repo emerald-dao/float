a92f7744d1f873f8e6a3ad8c903578bb51b2625e
Jacob Tucker <tsnakejake@aol.com>
2022-05-30 21:57:56 -0500

    fix time again lol im terrible

--- src/lib/components/ClaimButton.svelte
+++ src/lib/components/ClaimButton.svelte
@@ -31,7 +31,7 @@
 
   function claimTheFloat() {
     const timeOfEventCreation = floatEvent.dateCreated;
-    const timeOfUpdate = floatEvent.dateCreated;
+    const timeOfUpdate = 1653965861;
     if (timeOfEventCreation >= timeOfUpdate) {
       const secretSig = signWithClaimCode(claimCode);
       claimFLOATv2(floatEvent?.eventId, floatEvent?.host, secretSig);
