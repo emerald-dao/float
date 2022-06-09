64740285d5e735cb0433a60de1371ee57d186dff
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-06-09 12:32:57 +0200

    fixed that godawful x

--- src/lib/components/WalletModal.svelte
+++ src/lib/components/WalletModal.svelte
@@ -4,7 +4,11 @@
 </script>
 
 <article>
-  <button id="x" on:click={() => ($walletModal = false)}>x</button>
+  <button id="x" on:click={() => ($walletModal = false)}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+    </svg>
+  </button>
   <header>
     <h1>Choose your wallet</h1>
   </header>
@@ -30,15 +34,23 @@
 
 <style>
   #x {
-    position: fixed;
+    position: absolute;
     top: 5px;
     right: 5px;
-    width: 20px;
-    height: 20px;
+    width: 30px;
+    height: 30px;
     display: flex;
+    padding:0;
     justify-content: center;
     align-items: center;
-    padding: 15px;
+    background:none;
+  }
+
+  #x svg {
+    position:relative;
+    color:var(--primary);
+    width: 30px;
+    height:30px;
   }
 
   article {
