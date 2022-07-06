d97851ada63162110f6dd937aefaa9a56f95572e
jacob-tucker <jacobtucker818@gmail.com>
2022-07-06 13:32:43 -0400

    ban magic nft club

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,5 +3,6 @@ export const PAGE_TITLE_EXTENSION = '| FLOAT by Emerald City DAO'
 export const denylist = [
   "0x2671da731110abd5",
   "0xb2a8478aaed14644",
-  "0xe3b8f1469c159da0"
+  "0xe3b8f1469c159da0",
+  "0x5cd62ecc83793917"
 ] 
