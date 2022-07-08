4397b86f5dcb8b826d720d93c25e1f86b982f1ff
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-07-08 11:56:56 +0800

    chore: update vite cfg

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		vite: {
-			assetsInclude: ["**/*.cdc"],
-		}
+		adapter: adapter()
 	}
 };
 
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
+	assetsInclude: ["**/*.cdc"],
 	plugins: [sveltekit()]
 };
 export default config;
