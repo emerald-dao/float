67095f1b6f5447c944abb4c69cba14edba6a4f8e
jacob-tucker <jacobtucker818@gmail.com>
2022-07-27 23:35:08 -0400

    please fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		vite: {
-			define: {
-				global: {}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
