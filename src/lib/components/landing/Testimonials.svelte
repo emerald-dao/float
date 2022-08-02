cd964e0a4aad9b648998f63fc33b489aa0f5a1bd
Jacob <jacobtucker818@gmail.com>
2022-08-02 18:20:46 -0400

    new home page updates

--- src/lib/components/landing/Testimonials.svelte
+++ src/lib/components/landing/Testimonials.svelte
@@ -2,8 +2,8 @@
     <h2>Testimonials</h2>
     <div class="testimonials">
         <div>
-            <p>Emery provided a solid and structured foundation for my day to day job. Now all the data I need is one click away. I am able to make well-weighted decisions fast enough, to streamline more projects than I ever had.</p>
-            <small>Max K, Engineering Manager at Bumble</small>
+            <p>I was just gonna say, ahh man floats are fun, so easy and so much can be done with it! It is golddd</p>
+            <small>HumblePoom, founder of Driverz NFT.</small>
         </div>
         <div>
             <p>Now all the data I need is one click away. I am able to make well-weighted decisions fast enough, to streamline more projects than I ever had.</p>
--- src/lib/components/landing/UseCases.svelte
+++ src/lib/components/landing/UseCases.svelte
@@ -5,24 +5,49 @@
             description:
                 "Host an event on Discord and create an event for it. Have participants claim the FLOAT to represent their attendance.",
             image: "/discord-logo.png",
+            example: 'https://floats.city/andrea.find/event/187813244'
         },
         {
-            name: "Twitter Space AMA",
+            name: "Twitter Space",
             description:
                 "Host a Twitter Space and create an event for it. Have participants claim the FLOAT to represent their attendance.",
-            image: "/twitter-logo.webp",
+            image: "/twitter-logo.png",
+            example: 'https://floats.city/0x29d28c3660dbaead/event/252847127'
         },
         {
             name: "Giveaways",
             description:
                 "Have participants claim a FLOAT to enter the giveaway. Then, in Discord using the Emerald bot, use the /randomfloats command to pick a winner.",
             image: "/favicon.png",
+            example: 'https://floats.city/jacobmtucker.find/event/228533912'
         },
         {
-            name: "Giveaways",
+            name: "Fundraisers",
             description:
-                "Have participants claim a FLOAT to enter the giveaway. Then, in Discord using the Emerald bot, use the /randomfloats command to pick a winner.",
+                "Created a paid FLOAT and request that people purchase it for a fundraiser, while also claiming a FLOAT marking that they participated in the effort.",
             image: "/favicon.png",
+            example: 'https://floats.city/relief.find/event/191808682'
+        },
+        {
+            name: "Whitelist",
+            description:
+                "Have users claim a FLOAT to get onto a whitelist. You can download a list of addresses who claimed your FLOAT.",
+            image: "/favicon.png",
+            example: 'https://floats.city/robenoch.find/event/204594987'
+        },
+        {
+            name: "Merch Voucher",
+            description:
+                "Want to give out merch to people who get onto a list? Create a FLOAT and have people claim it to be able to purchase merch in the future.",
+            image: "/favicon.png",
+            example: 'https://floats.city/0x7b3cebdc058c1660/event/192961887'
+        },
+        {
+            name: "Quizzes",
+            description:
+                "Make a FLOAT Group and then add a bunch of different FLOAT events to that group, each with their own question. For each FLOAT event, make the secret code the answer to that quiz, and then use Emerald bot to validate if a user has all the FLOATs from the Group! ",
+            image: "/favicon.png",
+            example: 'https://floats.city/dene.find/group/Icons%20of%20Extreme%20Sports%20-%20QUIZ'
         },
     ];
 </script>
@@ -34,7 +59,10 @@
             <div>
                 <h3>{useCase.name}</h3>
                 <p>{useCase.description}</p>
-                <img src={useCase.image} alt="logo" />
+                <div class="at-bottom">
+                    <img src={useCase.image} alt="logo" />
+                    <a href={useCase.example}>See Example</a>
+                </div>
             </div>
         {/each}
     </div>
@@ -46,6 +74,7 @@
         font-weight: 600;
         line-height: 1.75rem;
         margin-bottom: 1rem;
+        color: var(--contrast-hover);
     }
     .use-cases {
         padding: 0 2rem;
@@ -70,21 +99,31 @@
         box-sizing: border-box;
         flex-basis: 30%;
         flex-grow: 1;
-        padding: 1.3rem 1.2rem 4rem;
+        padding: 1.3rem 1.2rem 1rem;
         background-color: var(--primary-hover);
         min-width: 300px;
+        display: flex;
+        flex-direction: column;
     }
 
     .use-cases-list > div > p {
+        color: var(--contrast);
         letter-spacing: -0.01rem;
         line-height: 120%;
         margin-bottom: 1rem;
+        flex-grow: 1;
     }
 
-    .use-cases-list > div > img {
+    .at-bottom {
+        display: flex;
+        position: relative;
+        justify-content: space-between;
+        align-items: center;
+        width:100%;
+    }
+
+    .at-bottom > img {
         max-height: 50px;
-        position: absolute;
-        left: 1.2rem;
-        bottom: 1.2rem;
+        object-fit: contain;
     }
 </style>
