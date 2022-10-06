21341f91481eb587676442b5b23eb53f13598c23
gregsantos <greg.santos@dapperlabs.com>
2022-10-06 15:07:36 -0700

    set global to globalThis to fix build issue

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 // vite.config.js
-import { sveltekit } from '@sveltejs/kit/vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import {sveltekit} from "@sveltejs/kit/vite"
+import {nodePolyfills} from "vite-plugin-node-polyfills"
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		nodePolyfills({
+  define: {
+    global: "globalThis",
+  },
+  plugins: [
+    nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
     }),
-		sveltekit()
-	]
-};
-export default config;
+    sveltekit(),
+  ],
+}
+export default config
