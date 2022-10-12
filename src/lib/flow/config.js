14588e7b37bbcea9bf6fafdef9a980c5241cb682
BT.Wood(Tang Bo Hao) <tech@btang.cn>
2022-10-12 15:08:23 +0800

    feat: add default address

--- src/lib/flow/config.js
+++ src/lib/flow/config.js
@@ -13,6 +13,9 @@ config({
   "0xFLOWSTORAGEFEES": "0xe467b9dd11fa00df"
 })
 
+const isTestnet = import.meta.env.VITE_FLOW_NETWORK === 'testnet'
+const floatEventSeriesAddress = import.meta.env.VITE_FLOAT_EVENTSERIES_ADDRESS || (isTestnet ? "0x78ba14dd1c817ec7" : "0x1dd5caae66e2c440")
+
 export const addressMap = {
   FlowToken: import.meta.env.VITE_FLOWTOKEN_ADDRESS,
   FungibleToken: import.meta.env.VITE_FUNGIBLETOKEN_ADDRESS,
@@ -21,13 +24,13 @@ export const addressMap = {
   FLOAT: import.meta.env.VITE_FLOAT_ADDRESS,
   FLOATVerifiers: import.meta.env.VITE_FLOAT_ADDRESS,
   GrantedAccountAccess: import.meta.env.VITE_FLOAT_ADDRESS,
-  FLOATEventSeries: import.meta.env.VITE_FLOAT_EVENTSERIES_ADDRESS,
-  FLOATEventSeriesViews: import.meta.env.VITE_FLOAT_EVENTSERIES_ADDRESS,
-  FLOATEventSeriesGoals: import.meta.env.VITE_FLOAT_EVENTSERIES_ADDRESS,
-  FLOATTreasuryStrategies: import.meta.env.VITE_FLOAT_EVENTSERIES_ADDRESS,
-  EmeraldPass: import.meta.env.VITE_EMERALD_PASS_ADDRESS,
-  NFTCatalog: import.meta.env.VITE_FLOW_NETWORK === 'testnet' ? "0x324c34e1c517e4db" : "0x49a7cda3a1eecc29",
-  NFTRetrieval: import.meta.env.VITE_FLOW_NETWORK === 'testnet' ? "0x324c34e1c517e4db" : "0x49a7cda3a1eecc29",
+  FLOATEventSeries: floatEventSeriesAddress,
+  FLOATEventSeriesViews: floatEventSeriesAddress,
+  FLOATEventSeriesGoals: floatEventSeriesAddress,
+  FLOATTreasuryStrategies: floatEventSeriesAddress,
+  EmeraldPass: import.meta.env.VITE_EMERALD_PASS_ADDRESS || (isTestnet ? "0x88b6d0be84df0918" : "0x6a07dbeb03167a13"),
+  NFTCatalog: isTestnet ? "0x324c34e1c517e4db" : "0x49a7cda3a1eecc29",
+  NFTRetrieval: isTestnet ? "0x324c34e1c517e4db" : "0x49a7cda3a1eecc29",
 }
 
 export const verifiersIdentifier = 'A.2d4c3caffbeab845';
