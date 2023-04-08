72b807f47c6ace856340e08546d2f980ed15b14e
jacob-tucker <jacobtucker818@gmail.com>
2023-04-08 15:46:09 -0500

    a16z page

---
+++ src/routes/a16z/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<img src="/a16z-css.png" alt="a16z CSS Banner" />
+
+<slot />
+
+<style>
+  img {
+    margin: 0 auto;
+    display: block;
+  }
+  .hero {
+    margin-top: 1rem;
+    position: relative;
+    text-align: center;
+  }
+
+  body {
+    --background-color: black !important;
+  }
+</style>
