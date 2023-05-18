9796b5a4a2512f3a69302e984b3512a9b6e1a01f
Jacob Tucker <tsnakejake@aol.com>
2023-05-18 14:44:02 -0500

    initial types

--- src/lib/types/float/float.interface.ts
+++ src/lib/types/float/float.interface.ts
@@ -1,1 +1,15 @@
 // FLOATs interfaces and types
+
+export interface FLOAT {
+  id: string;
+  dateReceived: string;
+  eventDescription: string;
+  eventHost: string;
+  eventId: string;
+  eventImage: string;
+  eventName: string;
+  originalRecipient: string;
+  serial: string;
+  totalSupply: string | null;
+  transferrable: boolean;
+}
