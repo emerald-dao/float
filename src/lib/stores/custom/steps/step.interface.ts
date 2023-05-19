e7a7cdb94a5c446015e5374a07bc6ea16b0db785
mateoroldos <mateoroldos19@gmail.com>
2023-05-19 14:42:39 -0300

    feat: Add Steps custom store

---
+++ src/lib/stores/custom/steps/step.interface.ts
@@ -0,0 +1,19 @@
+import type { ProgressStates } from '@emerald-dao/component-library/components/ProgressStep/progress-states.type';
+import type { SvelteComponent } from 'svelte';
+
+export interface Step {
+	name: string;
+	description?: string;
+	component: typeof SvelteComponent;
+	action: null | (() => Promise<ActionExecutionResult>);
+	state: ProgressStates;
+	button?: {
+		text: string;
+		icon?: string;
+	};
+}
+
+export interface ActionExecutionResult {
+	state: 'success' | 'error';
+	errorMessage: string;
+}
