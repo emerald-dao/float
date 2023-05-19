3ba3adc8a193d7af18da027538f049d7df444aef
mateoroldos <mateoroldos19@gmail.com>
2023-05-19 12:51:53 -0300

    refactor: Comment cleanup

--- src/lib/types/float/float.interface.ts
+++ src/lib/types/float/float.interface.ts
@@ -1,15 +1,13 @@
-// FLOATs interfaces and types
-
 export interface FLOAT {
-  id: string;
-  dateReceived: string;
-  eventDescription: string;
-  eventHost: string;
-  eventId: string;
-  eventImage: string;
-  eventName: string;
-  originalRecipient: string;
-  serial: string;
-  totalSupply: string | null;
-  transferrable: boolean;
+	id: string;
+	dateReceived: string;
+	eventDescription: string;
+	eventHost: string;
+	eventId: string;
+	eventImage: string;
+	eventName: string;
+	originalRecipient: string;
+	serial: string;
+	totalSupply: string | null;
+	transferrable: boolean;
 }
