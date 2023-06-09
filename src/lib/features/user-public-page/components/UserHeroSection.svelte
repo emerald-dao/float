432d2f258560fb139d55e5f7b02738c02248d51b
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-06-09 16:17:51 -0300

    User Public page implementation

---
+++ src/lib/features/user-public-page/components/UserHeroSection.svelte
@@ -0,0 +1,65 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<section class="container-small">
+	<img src="/test-toucan.png" alt="float" />
+	<h1 class="medium">mateor.find</h1>
+	<div class="row-7">
+		<div class="row media-wrapper">
+			<Icon icon="tabler:brand-twitter" width="22" height="22" />
+			<p>@___mateor</p>
+		</div>
+		<div class="row media-wrapper">
+			<Icon icon="tabler:brand-twitter" width="22" height="22" />
+			<p>@___mateor</p>
+		</div>
+		<div class="row media-wrapper">
+			<Icon icon="tabler:brand-twitter" width="22" height="22" />
+			<p>@___mateor</p>
+		</div>
+	</div>
+	<div class="stats-wrapper">
+		<div>
+			<p class="large">26</p>
+			<p class="small">FLOATs claimed</p>
+		</div>
+		<div>
+			<p class="large">7</p>
+			<p class="small">Events created</p>
+		</div>
+	</div>
+</section>
+
+<style type="scss">
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: var(--space-6);
+
+		img {
+			width: 260px;
+			height: 260px;
+			border-radius: 50%;
+		}
+
+		.media-wrapper {
+			gap: var(--space-1);
+			border: 1px solid var(--clr-border-primary);
+			padding: var(--space-2) var(--space-3);
+			border-radius: var(--radius-3);
+		}
+
+		.stats-wrapper {
+			width: 100%;
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			text-align: center;
+			border-top: 1px dashed var(--clr-border-primary);
+			border-bottom: 1px dashed var(--clr-border-primary);
+			padding: var(--space-5) 0 var(--space-5) 0;
+		}
+	}
+</style>
---
+++ src/routes/user/[username]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import UserPublicPage from '$lib/features/user-public-page/UserPublicPage.svelte';
+</script>
+
+<UserPublicPage />
