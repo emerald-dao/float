80c7c87169f21e7f9caf705ebdaaac5f2e06bf8b
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-06-09 16:14:50 -0300

    Creation of EventTypeEnum

---
+++ src/lib/types/content/metadata/event-types.enum.ts
@@ -0,0 +1,14 @@
+export enum EventTypeEnum {
+	Online = 'online course',
+	YouTube = 'youtube',
+	Sport = 'sport event',
+	LiveStreaming = 'live streaming',
+	Webinar = 'webinar',
+	Workshop = 'workshop',
+	DiscordAMA = 'discord AMA',
+	Conference = 'conference',
+	Twitter = 'twitter space',
+	BirthdayParty = 'birthday party',
+	OnsiteCourse = 'onsite course',
+	ProductLunch = 'product lunch'
+}
