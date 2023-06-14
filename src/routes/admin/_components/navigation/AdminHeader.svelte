be62b2fef563378b6e63bfa902a57648785e3f53
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-06-14 18:01:33 -0300

    Admin Header and Dashboard-Nav component

---
+++ src/routes/admin/_components/navigation/AdminHeader.svelte
@@ -0,0 +1,29 @@
+<script type="ts">
+	export let route: string | null;
+</script>
+
+<div class="header-wrapper row">
+	<img src="/test-toucan.png" alt="float" />
+	<p>mateor.find / {route}</p>
+</div>
+
+<style type="scss">
+	.header-wrapper {
+		padding: var(--space-3) var(--space-10);
+		align-items: center;
+		top: 10px;
+		z-index: 1000;
+		background-color: var(--clr-background-primary);
+		gap: var(--space-4);
+
+		img {
+			width: 56px;
+			height: 56px;
+			border-radius: 50%;
+		}
+
+		p {
+			color: var(--clr-heading-main);
+		}
+	}
+</style>
