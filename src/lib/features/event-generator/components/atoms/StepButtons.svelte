d8f5d355e5b3f130fbe1a0046e542b987160a7ba
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-07-04 16:08:34 -0300

    StepButton component cleaned

--- src/lib/features/event-generator/components/atoms/StepButtons.svelte
+++ src/lib/features/event-generator/components/atoms/StepButtons.svelte
@@ -3,21 +3,10 @@
 	import type { createActiveStep } from '$stores/custom/steps/ActiveStep';
 	import type { createSteps } from '$stores/custom/steps/Steps';
 	import Icon from '@iconify/svelte';
-	import captureDomToPng from '$lib/utilities/captureDomToPng';
 
 	export let activeStepStore: ReturnType<typeof createActiveStep>;
 	export let stepsStore: ReturnType<typeof createSteps>;
 	export let stepDataValid: boolean;
-
-	export const captureFloat = async () => {
-		let capturedImageSrc: string;
-
-		let elementToCapture = document.getElementById('target-element');
-		let poweredByStyle = document.getElementById('powered-by-style');
-		let titleStyle = document.getElementById('title-style');
-
-		capturedImageSrc = await captureDomToPng(elementToCapture, poweredByStyle, titleStyle);
-	};
 </script>
 
 <div class="row-space-between">
@@ -29,28 +18,16 @@
 			</Button>
 		{/if}
 	</div>
-
-	{#if $stepsStore[$activeStepStore].button.text !== 'Create Event'}
-		<Button
-			on:click={() => activeStepStore.increment()}
-			size="large"
-			state={stepDataValid ? 'active' : 'disabled'}
-		>
-			{#if $stepsStore[$activeStepStore].button}
-				{$stepsStore[$activeStepStore].button.text}
-			{:else}
-				Next
-			{/if}
-			<Icon icon={$stepsStore[$activeStepStore].button.icon ?? 'tabler:arrow-right'} />
-		</Button>
-	{:else}
-		<Button on:click={captureFloat} size="large" state={stepDataValid ? 'active' : 'disabled'}>
-			{#if $stepsStore[$activeStepStore].button}
-				{$stepsStore[$activeStepStore].button.text}
-			{:else}
-				Next
-			{/if}
-			<Icon icon={$stepsStore[$activeStepStore].button.icon ?? 'tabler:arrow-right'} />
-		</Button>
-	{/if}
+	<Button
+		on:click={() => activeStepStore.increment()}
+		size="large"
+		state={stepDataValid ? 'active' : 'disabled'}
+	>
+		{#if $stepsStore[$activeStepStore].button}
+			{$stepsStore[$activeStepStore].button.text}
+		{:else}
+			Next
+		{/if}
+		<Icon icon={$stepsStore[$activeStepStore].button.icon ?? 'tabler:arrow-right'} />
+	</Button>
 </div>
