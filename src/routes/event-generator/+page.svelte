eabaed3a27f2ec75fc9ded8ddbf602aa34143336
mateoroldos <mateoroldos19@gmail.com>
2023-07-11 11:43:59 -0300

    feat: Add ConnectWalletPage to event generator

--- src/routes/event-generator/+page.svelte
+++ src/routes/event-generator/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { logIn, unauthenticate } from '$flow/actions';
 	import EventGenerator from '$lib/features/event-generator/EventGenerator.svelte';
 	import { user } from '$stores/flow/FlowStore';
+	import { ConnectWalletPage } from '@emerald-dao/component-library';
 </script>
 
 {#if $user && $user.addr}
 	<EventGenerator />
 {:else}
-	<span>Connect wallet</span>
+	<ConnectWalletPage user={$user} {logIn} {unauthenticate} />
 {/if}
