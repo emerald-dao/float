b07a4961e701895efcd032d75a2e85de1f720c7b
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-08-18 16:56:45 -0300

    fix: home FloatTicket with data from config

---
+++ src/lib/config/homeFloatTicket.ts
@@ -0,0 +1,22 @@
+import type { FLOAT } from '$lib/types/float/float.interface';
+
+const homeFloatTicket: FLOAT = {
+	id: '123456789',
+	originalRecipient: '0x99dd9933jj99k0',
+	eventName: 'Emerald City',
+	eventType: 'hackathon',
+	eventHost: 'Emerald DAO',
+	dateReceived: '1649886952.00000000',
+	eventDescription: 'Hackathon dictated by Emerald City',
+	eventId: '941533131',
+	eventImage:
+		'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
+	eventLogo:
+		'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
+
+	serial: '12',
+	totalSupply: null,
+	transferrable: false
+};
+
+export default homeFloatTicket;
--- src/routes/_components/sections/HeroSection.svelte
+++ src/routes/_components/sections/HeroSection.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Blur from '$lib/components/Blur.svelte';
 	import FloatTicket from '$lib/components/floats/FloatTicket.svelte';
+	import homeFloatTicket from '$lib/config/homeFloatTicket';
 	import { Button } from '@emerald-dao/component-library';
 </script>
 
@@ -14,26 +15,8 @@
 			trust for event creators.
 		</p>
 	</div>
-	<FloatTicket
-		float={{
-			id: '123456789',
-			originalRecipient: '0x99dd9933jj99k0',
-			eventName: 'Emerald City',
-			eventType: 'hackathon',
-			eventHost: 'Emerald DAO',
-			dateReceived: '1649886952.00000000',
-			eventDescription: 'Hackathon dictated by Emerald City',
-			eventId: '941533131',
-			eventImage:
-				'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
-			eventLogo:
-				'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
 
-			serial: 'NO IDEA',
-			totalSupply: null,
-			transferrable: false
-		}}
-	/>
+	<FloatTicket float={homeFloatTicket} />
 	<div class="column-2 center">
 		<Button color="primary" size="large" width="extended" href="/event-generator"
 			>Create an Event - it's free!</Button
