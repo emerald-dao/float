1afed5983fb2dab7619db9a47940031e6395c7fc
mateoroldos <mateoroldos19@gmail.com>
2023-09-05 14:43:02 -0300

    feat: Add seed event to db

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -8,3 +8,8 @@ insert into
 floats_groups (float_id, group_id)
 values
 ('187900113', 1), ('196985252', 1), ('242885523', 2);
+
+insert into
+events (id, creator_address)
+values
+('187813244', '0x99bd48c8036e2876');
