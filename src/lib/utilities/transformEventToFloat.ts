651e8ff1468fdf5f204955e8595eab5f80b4dd21
mateoroldos <mateoroldos19@gmail.com>
2023-09-06 11:43:23 -0300

    feat: Improve transformEventToFloat function

--- src/lib/utilities/transformEventToFloat.ts
+++ src/lib/utilities/transformEventToFloat.ts
@@ -1,22 +1,19 @@
 import type { Event } from '$lib/types/event/event.interface';
 import type { FLOAT } from '$lib/types/float/float.interface';
+import { unixTimestampToFormattedDate } from './dates/unixTimestampToFormattedDate';
 
 function transformEventToFloat(event: Event): FLOAT {
 	const float: FLOAT = {
 		id: event.eventId,
-		dateReceived: new Date().toLocaleString('default', {
-			month: 'numeric',
-			day: 'numeric',
-			year: 'numeric'
-		}),
+		dateReceived: `${Date.now() / 1000}`,
 		eventDescription: event.description,
 		eventHost: event.host,
 		eventId: event.eventId,
 		eventImage: event.image,
 		eventLogo: event.eventLogo,
 		eventName: event.name,
-		originalRecipient: 'Jacob Tucker',
-		serial: '0001',
+		originalRecipient: '0x99bd48c8036e2876',
+		serial: '1',
 		totalSupply: event.totalSupply,
 		transferrable: event.transferrable,
 		eventType: event.eventType
