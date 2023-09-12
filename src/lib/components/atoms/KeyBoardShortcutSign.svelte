0cc355c538afb90f0986f3df6fb674be5530d356
mateoroldos <mateoroldos19@gmail.com>
2023-09-12 11:15:14 -0300

    feat: Searchbar improvements

---
+++ src/lib/components/atoms/KeyBoardShortcutSign.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let symbol: string;
+	export let fontSize = '1rem';
+</script>
+
+<div style={`font-size: ${fontSize}`}>
+	<p>{symbol}</p>
+</div>
+
+<style lang="scss">
+	div {
+		p {
+			min-width: 1.7em;
+			background-color: var(--clr-neutral-badge);
+			padding: 0.3em 0.3em;
+			border-radius: 0.23em;
+			opacity: 0.7;
+			font-size: 1em;
+			color: var(--clr-text-main);
+			border: 1px solid var(--clr-neutral-badge);
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			line-height: 1.1em;
+		}
+	}
+</style>
---
+++ src/lib/utilities/clickOutside.ts
@@ -0,0 +1,19 @@
+/** Dispatch event on click outside of node */
+export function clickOutside(element, callbackFunction) {
+	function onClick(event) {
+		if (!element.contains(event.target)) {
+			callbackFunction();
+		}
+	}
+
+	document.body.addEventListener('click', onClick);
+
+	return {
+		update(newCallbackFunction) {
+			callbackFunction = newCallbackFunction;
+		},
+		destroy() {
+			document.body.removeEventListener('click', onClick);
+		}
+	};
+}
