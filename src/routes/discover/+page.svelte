2afcd55ad7d5de55e91e816a1637f2a0b7382ae9
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-09-12 15:16:53 -0300

    fixes on discover page (on searchbar and trending)

--- src/routes/discover/+page.svelte
+++ src/routes/discover/+page.svelte
@@ -1,30 +1,19 @@
 <script lang="ts">
-	import { Button } from '@emerald-dao/component-library';
-	import validateUserExistance from './_actions/validateUserExistance.js';
-	import Icon from '@iconify/svelte';
-	import { fly } from 'svelte/transition';
-	import TrendingEvents from './_components/TrendingEvents.svelte';
 	import { onMount } from 'svelte';
 	import type { EventWithStatus } from '$lib/types/event/event.interface.js';
 	import getTrendingEventsFromBlockchain from './_actions/getTrendingEventsFromBlockchain.js';
-
-	let userValidation: boolean;
-	let inputValue: string;
-
-	const handleSubmit = async (value: string) => {
-		userValidation = await validateUserExistance(value);
-
-		if (userValidation) {
-			window.location.href = `u/${value}`;
-		}
-	};
+	import EventCard from '$lib/components/events/EventCard.svelte';
 
 	let dataFetched = false;
+	let dataLoading = true;
 	let trendingEvents: EventWithStatus[];
 
 	onMount(async () => {
 		trendingEvents = await getTrendingEventsFromBlockchain();
-		dataFetched = true;
+		dataLoading = false;
+		if (trendingEvents) {
+			dataFetched = true;
+		}
 	});
 </script>
 
@@ -36,67 +25,69 @@
     rgba(250, 250, 250, 1)
   ), url("/badges/each-event-type-floats/level-3.png")`}
 >
-	<div class="container-large">
-		<h1 class="w-medium">Discover</h1>
-		<div class="search-bar-and-message-wrapper column-1 justify-center align-center">
-			<div class="input-message">
-				{#if userValidation === false}
-					<div
-						class="row-1 align-center justify-center"
-						class:alert={!userValidation}
-						in:fly={{ duration: 300, y: -10 }}
-					>
-						<Icon icon="tabler:lock" />
-						<span class="small">User doesn't exist</span>
-					</div>
-				{/if}
+	<div class="container-medium">
+		<h2 class="w-medium">🔥Trending Events🔥</h2>
+
+		{#if dataLoading}
+			<div class="empty-state">
+				<span><em>Loading trending events</em></span>
 			</div>
-			<form class="search-bar-wrapper row-1">
-				<input type="text" placeholder="Type address or .find name" bind:value={inputValue} />
-				<Button width="extended" size="small" on:click={() => handleSubmit(inputValue)}>
-					Go
-					<Icon icon="tabler:arrow-right" />
-				</Button>
-			</form>
-		</div>
+		{:else if dataFetched}
+			<div class="cards-wrapper">
+				{#each trendingEvents as event}
+					<div class="event-wrapper">
+						<EventCard {event} display="grid" displayedInAdmin={false} hasLink={false} />
+					</div>
+				{/each}
+			</div>
+		{:else}
+			<div class="empty-state">
+				<span><em>No trending events</em></span>
+			</div>
+		{/if}
 	</div>
-</section>
-<section class="container-medium">
-	{#if dataFetched}
-		<TrendingEvents events={trendingEvents} />
-	{/if}
 </section>
 
 <style lang="scss">
-	.container-large {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		gap: var(--space-6);
+	section {
+		min-height: 85vh;
 
-		.search-bar-and-message-wrapper {
-			width: 100%;
+		.container-medium {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
 
-			.search-bar-wrapper {
-				width: 100%;
-				display: flex;
+			h2 {
+				text-align: center;
+				margin-bottom: var(--space-15);
+			}
+			.cards-wrapper {
+				display: grid;
+				grid-template-columns: 1fr;
 				justify-content: center;
+				align-items: center;
+				gap: var(--space-8);
+				width: 100%;
 
-				input {
-					max-width: 250px;
+				@include mq(small) {
+					grid-template-columns: 1fr 1fr;
 				}
 
-				.input-message {
-					justify-content: center;
-					align-items: center;
-					font-size: var(--font-size-1);
-					padding-left: var(--space-2);
-
-					.alert {
-						color: var(--clr-alert-main);
-					}
+				@include mq(medium) {
+					grid-template-columns: 1fr 1fr 1fr;
 				}
+
+				.event-wrapper {
+					padding-bottom: var(--space-3);
+				}
+			}
+			.empty-state {
+				width: 100%;
+				height: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: center;
 			}
 		}
 	}
