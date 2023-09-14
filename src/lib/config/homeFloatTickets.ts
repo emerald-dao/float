74b70e2cd1f3e5bf578f49006b16a42cc6d5fa61
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-09-14 11:29:41 -0300

    Make hero float random type

---
+++ src/lib/config/homeFloatTickets.ts
@@ -0,0 +1,63 @@
+import type { FLOAT } from '$lib/types/float/float.interface';
+
+const homeFloatTickets: FLOAT[] = [
+	{
+		id: '123456789',
+		originalRecipient: '0x11ca36743554b4b0',
+		eventName: 'Emerald City Discord Meeting',
+		eventType: 'hackathon',
+		eventHost: '0x99bd48c8036e2876',
+		dateReceived: '1649886952.00000000',
+		eventDescription: 'Hackathon dictated by Emerald City',
+		eventId: '941533131',
+		eventImage:
+			'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
+		eventLogo:
+			'https://cdn.discordapp.com/attachments/1054775421671055390/1105958730799005876/Mateo_Roldos_An_avatar_for_the_Emerald_City_student._Emerald_Ci_0d9c4a87-8861-4779-bd9a-080ef0c838a5.png',
+
+		serial: '12',
+		totalSupply: null,
+		transferrable: false,
+		extraMetadata: { medalType: 'gold' }
+	},
+	{
+		id: '123456789',
+		originalRecipient: '0x11ca36743554b4b0',
+		eventName: 'Emerald City Discord Meeting',
+		eventType: 'course',
+		eventHost: '0x99bd48c8036e2876',
+		dateReceived: '1649886952.00000000',
+		eventDescription: 'Hackathon dictated by Emerald City',
+		eventId: '941533131',
+		eventImage:
+			'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
+		eventLogo:
+			'https://cdn.discordapp.com/attachments/1054775421671055390/1105958730799005876/Mateo_Roldos_An_avatar_for_the_Emerald_City_student._Emerald_Ci_0d9c4a87-8861-4779-bd9a-080ef0c838a5.png',
+
+		serial: '12',
+		totalSupply: null,
+		transferrable: false,
+		extraMetadata: { medalType: null }
+	},
+	{
+		id: '123456789',
+		originalRecipient: '0x11ca36743554b4b0',
+		eventName: 'Emerald City Discord Meeting',
+		eventType: 'discordMeeting',
+		eventHost: '0x99bd48c8036e2876',
+		dateReceived: '1649886952.00000000',
+		eventDescription: 'Hackathon dictated by Emerald City',
+		eventId: '941533131',
+		eventImage:
+			'https://cdn.discordapp.com/attachments/1054775421671055390/1105958725711319201/tsnakejake_A_cartoon_man_reading_a_mystical_book_with_an_emeral_d5f03067-6692-4152-8ade-37621c0776b5.png',
+		eventLogo:
+			'https://cdn.discordapp.com/attachments/1054775421671055390/1105958730799005876/Mateo_Roldos_An_avatar_for_the_Emerald_City_student._Emerald_Ci_0d9c4a87-8861-4779-bd9a-080ef0c838a5.png',
+
+		serial: '12',
+		totalSupply: null,
+		transferrable: false,
+		extraMetadata: { medalType: null }
+	}
+];
+
+export default homeFloatTickets;
