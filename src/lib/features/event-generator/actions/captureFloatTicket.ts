0189a8b620bbb595a9795cdd86349f04e3f4be53
mateoroldos <mateoroldos19@gmail.com>
2023-09-26 12:42:06 -0300

    fix: Clean captureFloatTicket

--- src/lib/features/event-generator/actions/captureFloatTicket.ts
+++ src/lib/features/event-generator/actions/captureFloatTicket.ts
@@ -15,18 +15,20 @@ const captureFloatTicket = async (elementToCapture: HTMLElement): Promise<File |
 			width: elementToCapture.offsetWidth * scale
 		});
 
-		const imageDataUrl = await domtoimage.toPng(elementToCapture, {
-			filter: (node: Node) => (node as HTMLElement).id !== 'float-back',
-			height: elementToCapture.offsetHeight * scale,
-			style: {
-				transform: `scale(${scale}) translate(${elementToCapture.offsetWidth / 2 / scale}px, ${
-					elementToCapture.offsetHeight / 2 / scale
-				}px)`
-			},
-			width: elementToCapture.offsetWidth * scale
-		});
+		// IN ORDER TO GET THE DATA URL OF THE IMAGE, AND SEE HOW THE SCREENSHOT IS LOOKING, UNCOMMENT CODE BELOW
 
-		console.log('a', imageDataUrl);
+		// const imageDataUrl = await domtoimage.toPng(elementToCapture, {
+		// 	filter: (node: Node) => (node as HTMLElement).id !== 'float-back',
+		// 	height: elementToCapture.offsetHeight * scale,
+		// 	style: {
+		// 		transform: `scale(${scale}) translate(${elementToCapture.offsetWidth / 2 / scale}px, ${
+		// 			elementToCapture.offsetHeight / 2 / scale
+		// 		}px)`
+		// 	},
+		// 	width: elementToCapture.offsetWidth * scale
+		// });
+
+		// console.log(imageDataUrl);
 
 		return new File([imageBlob], 'float-ticket.png', { type: 'image/png' });
 	} catch (error) {
