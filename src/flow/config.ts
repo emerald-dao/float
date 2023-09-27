baa6fa8d80bc9c572929d12389d7de4e0c52c869
jacob-tucker <jacobtucker818@gmail.com>
2023-09-27 17:23:04 -0400

    add Ledger and DW on testnet & mainnet

--- src/flow/config.ts
+++ src/flow/config.ts
@@ -23,12 +23,12 @@ const fclConfigInfo = {
 	testnet: {
 		accessNode: 'https://rest-testnet.onflow.org',
 		discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
-		discoveryAuthInclude: []
+		discoveryAuthInclude: ["0x82ec283f88a62e65", "0x9d2e44203cb13051"]
 	},
 	mainnet: {
 		accessNode: 'https://rest-mainnet.onflow.org',
 		discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
-		discoveryAuthInclude: ["0xead892083b3e2c6c"]
+		discoveryAuthInclude: ["0xead892083b3e2c6c", "0xe5cd26afebe62781"]
 	}
 };
 
@@ -39,5 +39,5 @@ config({
 	'flow.network': network,
 	'accessNode.api': fclConfigInfo[network].accessNode,
 	'discovery.wallet': fclConfigInfo[network].discoveryWallet,
-	"discovery.authn.include": fclConfigInfo[network].discoveryAuthInclude, // include Dapper Wallet
+	"discovery.authn.include": fclConfigInfo[network].discoveryAuthInclude, // include Dapper Wallet and Ledger
 });
