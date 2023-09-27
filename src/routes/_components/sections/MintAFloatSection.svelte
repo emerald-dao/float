d355c4d6b8aca54562e1c4aec523c1c34a882a1f
jacob-tucker <jacobtucker818@gmail.com>
2023-09-27 01:54:33 -0400

    temporary welcome float

--- src/routes/_components/sections/MintAFloatSection.svelte
+++ src/routes/_components/sections/MintAFloatSection.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import Blur from '$lib/components/Blur.svelte';
 	import EventQrCard from '$lib/components/EventQRCard.svelte';
+	import { onMount } from 'svelte';
 	import SectionHeading from './atoms/SectionHeading.svelte';
+	import { page } from '$app/stores';
+	import { network } from '$flow/config';
+	import generateQRCode from '$lib/utilities/generateQRCode';
+
+	let eventId: string = network === 'mainnet' ? '' : network === 'testnet' ? '173143134' : '';
+	let username: string =
+		network === 'mainnet' ? '' : network === 'testnet' ? '0xd7f69a06f10eae0e' : '';
+	let qrCodeDataUrl = '';
+
+	onMount(async () => {
+		let qr = `${$page.url.origin}/event/${username}/${eventId}`;
+		qrCodeDataUrl = await generateQRCode(qr);
+	});
 </script>
 
 <section class="container-small center column-10">
@@ -11,7 +25,7 @@
 		title="Start collecting experiences"
 		description="Mint this FLOAT to start your collection!"
 	/>
-	<EventQrCard title="I visited FLOAT's Home Page" type="Website Visit" />
+	<EventQrCard title="I visited FLOAT's Home Page" type="Website Visit" qrUrl={qrCodeDataUrl} />
 </section>
 
 <style lang="scss">
