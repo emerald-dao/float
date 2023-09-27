b5edd0fb592365eaa8b66c5536488d25a82b665d
jacob-tucker <jacobtucker818@gmail.com>
2023-09-27 17:16:35 -0400

    add DW on mainnet

--- src/flow/config.ts
+++ src/flow/config.ts
@@ -17,15 +17,18 @@ export const network: 'mainnet' | 'testnet' | 'emulator' = env.PUBLIC_FLOW_NETWO
 const fclConfigInfo = {
 	emulator: {
 		accessNode: 'http://127.0.0.1:8888',
-		discoveryWallet: 'http://localhost:8701/fcl/authn'
+		discoveryWallet: 'http://localhost:8701/fcl/authn',
+		discoveryAuthInclude: []
 	},
 	testnet: {
 		accessNode: 'https://rest-testnet.onflow.org',
-		discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn'
+		discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+		discoveryAuthInclude: []
 	},
 	mainnet: {
 		accessNode: 'https://rest-mainnet.onflow.org',
-		discoveryWallet: 'https://fcl-discovery.onflow.org/authn'
+		discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+		discoveryAuthInclude: ["0xead892083b3e2c6c"]
 	}
 };
 
@@ -35,5 +38,6 @@ config({
 	'fcl.accountProof.resolver': resolver,
 	'flow.network': network,
 	'accessNode.api': fclConfigInfo[network].accessNode,
-	'discovery.wallet': fclConfigInfo[network].discoveryWallet
+	'discovery.wallet': fclConfigInfo[network].discoveryWallet,
+	"discovery.authn.include": fclConfigInfo[network].discoveryAuthInclude, // include Dapper Wallet
 });
