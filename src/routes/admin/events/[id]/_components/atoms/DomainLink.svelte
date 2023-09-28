88bd0dbc39b6fd4921b24a86b875e5bfc0522993
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2023-09-28 10:59:03 -0300

    trying to fix input for ios

--- src/routes/admin/events/[id]/_components/atoms/DomainLink.svelte
+++ src/routes/admin/events/[id]/_components/atoms/DomainLink.svelte
@@ -52,12 +52,18 @@
 	</div>
 	<div class="row-3 link-wrapper">
 		{#if linkType === 'eventPage'}
-			<input class="link" disabled={true} value={`${$page.url.origin}/event/${$user.addr}/${id}`} />
+			<input
+				class="link"
+				type="text"
+				disabled={true}
+				placeholder={`${$page.url.origin}/event/${$user.addr}/${id}`}
+			/>
 		{:else if linkType === 'qr'}
 			<input
 				class="link"
+				type="text"
 				disabled={true}
-				value={`${$page.url.origin}/event/${$user.addr}/${id}/qr`}
+				placeholder={`${$page.url.origin}/event/${$user.addr}/${id}/qr`}
 			/>
 		{/if}
 		<div class="row-3">
@@ -109,7 +115,6 @@
 				padding: var(--space-1) var(--space-3);
 				overflow: hidden;
 				font-size: var(--font-size-0);
-				color: var(--clr-text-off);
 			}
 
 			.row-3 {
