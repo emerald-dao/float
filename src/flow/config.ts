691c66808ade49fa93bab10832c4ee67f356a6c5
jacob-tucker <jacobtucker818@gmail.com>
2023-10-14 19:59:13 -0400

    fix: discoveryauthnendpoint problem

--- src/flow/config.ts
+++ src/flow/config.ts
@@ -19,16 +19,19 @@ const fclConfigInfo = {
 	emulator: {
 		accessNode: 'http://127.0.0.1:8888',
 		discoveryWallet: 'http://localhost:8701/fcl/authn',
+		discoveryAuthnEndpoint: '',
 		discoveryAuthInclude: []
 	},
 	testnet: {
 		accessNode: 'https://rest-testnet.onflow.org',
 		discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+		discoveryAuthnEndpoint: 'https://fcl-discovery.onflow.org/api/testnet/authn',
 		discoveryAuthInclude: ["0x82ec283f88a62e65", "0x9d2e44203cb13051"]
 	},
 	mainnet: {
 		accessNode: 'https://rest-mainnet.onflow.org',
 		discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+		discoveryAuthnEndpoint: 'https://fcl-discovery.onflow.org/api/authn',
 		discoveryAuthInclude: ["0xead892083b3e2c6c", "0xe5cd26afebe62781"]
 	}
 };
@@ -39,6 +42,7 @@ fcl.config({
 	'fcl.accountProof.resolver': resolver,
 	'flow.network': network,
 	'accessNode.api': fclConfigInfo[network].accessNode,
+	'discovery.authn.endpoint': fclConfigInfo[network].discoveryAuthnEndpoint,
 	'discovery.wallet': fclConfigInfo[network].discoveryWallet,
 	// include Dapper Wallet and Ledger. 
 	// Docs: https://developers.flow.com/tools/clients/fcl-js/api#more-configuration
