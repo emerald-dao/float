6051e50f5c82e433e3fdf077544ad040ee7eb729
jacob-tucker <jacobtucker818@gmail.com>
2023-11-24 17:53:00 -0500

    remove: description 320 limit validation

--- src/lib/features/event-generator/components/steps/1-EventBasicData/validation.ts
+++ src/lib/features/event-generator/components/steps/1-EventBasicData/validation.ts
@@ -12,10 +12,6 @@ const validationSuite = create((data = {}, currentField?) => {
 	test('event-name', 'Event name must be at least 3 characters long', () => {
 		enforce(data.name).longerThan(3);
 	});
-
-	test('event-description', 'Event description should be shorter than 320 chars', () => {
-		enforce(data.description).shorterThan(320);
-	});
 });
 
 export default validationSuite;
